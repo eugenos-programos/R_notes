f65d293c1c3811a37fe1c83135c2dcd818637dcd
Yauheni Samchuk <71227355+eugenos-programos@users.noreply.github.com>
2022-05-17 23:47:21 +0300

    create complete function

--- Programming Assignment 1/air_pollution.r
+++ Programming Assignment 1/air_pollution.r
@@ -17,3 +17,19 @@ pollutantmean("specdata", "sulfate", 1:10)
 pollutantmean("specdata", "nitrate", 70:72)
 pollutantmean("specdata", "sulfate", 34)
 pollutantmean("specdata", "nitrate")
+
+complete <- function(directory, id = 1:332){
+    file_list <- list.files(path="../input/programming-assignment-1-air-pollution/specdata", pattern='csv', all.files=TRUE, full.names=TRUE)
+    file_list <- file_list[id]
+    nobs = c()
+    for (file_name in file_list){
+        data <- read.csv(file_name)
+        nobs = append(nobs, sum(!is.na(data[, 2]) & !is.na(data[, 3])))
+    }
+    data.frame(id, nobs)
+}
+
+cc <- complete("specdata", c(6, 10, 20, 34, 100, 200, 310))
+print(cc$nobs)
+cc <- complete("specdata", 54)
+print(cc$nobs)
